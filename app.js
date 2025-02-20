e738f85f8de33078951e371f611d26a2dd343a6c
Joan_Montanya <2023_joan.montanya@iticbnc.cat>
2025-02-20 18:54:09 +0100

    Mapa i altres

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-class Puntinteres{
-    static totalTasques =0;
-    constructor(id,esManual,ciutat,nom,direccio,tipus,latitud,longitud,puntuacio){
+//Definim les classes
+class Puntinteres {
+    static totalTasques = 0;
+    constructor(id, esManual, ciutat, nom, direccio, tipus, latitud, longitud, puntuacio) {
         this.id = id
         this.esManual = esManual
         this.ciutat = ciutat
@@ -11,62 +12,113 @@ class Puntinteres{
         this.longitud = longitud
         this.puntuacio = puntuacio
     }
-    get id(){
+    get id() {
         return this.id
     }
-    set id(valor){
+    set id(valor) {
         this.id = valor
     }
-    get esManual(){
+    get esManual() {
         return this.esManual
     }
-    set esManual(valor){
+    set esManual(valor) {
         this.esManual = valor
     }
-    static obtenirtotalElements(){
+    static obtenirtotalElements() {
         return Puntinteres.totalTasques
     }
 }
 
-class Atraccio extends Puntinteres{
-    constructor(id,esManual,ciutat,nom,direccio,tipus,latitud,longitud,puntuacio,horaris,preu){
-        super(id,esManual,ciutat,nom,direccio,tipus,latitud,longitud,puntuacio)
+class Atraccio extends Puntinteres {
+    constructor(id, esManual, ciutat, nom, direccio, tipus, latitud, longitud, puntuacio, horaris, preu, moneda) {
+        super(id, esManual, ciutat, nom, direccio, tipus, latitud, longitud, puntuacio)
         this.horaris = horaris
         this.preu = preu
+        this.moneda = moneda
     }
 
-    getPreuIva(){
-        return this.preu * 0,24;
+    getPreuIva() {
+        return this.preu * 0.24;
     }
 }
 
-class Museu extends Puntinteres{
-    constructor(id,esManual,ciutat,nom,direccio,tipus,latitud,longitud,puntuacio,horaris,preu,descripcio){
-        super(id,esManual,ciutat,nom,direccio,tipus,latitud,longitud,puntuacio)
+class Museu extends Puntinteres {
+    constructor(id, esManual, ciutat, nom, direccio, tipus, latitud, longitud, puntuacio, horaris, preu, descripcio, moneda) {
+        super(id, esManual, ciutat, nom, direccio, tipus, latitud, longitud, puntuacio)
         this.horaris = horaris
         this.preu = preu
         this.descripcio = descripcio
+        this.moneda = moneda
     }
 
-    getPreuIva(){
-        return this.preu * 0,24
+    getPreuIva() {
+        return this.preu * 0.24
     }
 }
 
-class Map{
-    constructor(){
+class Mapa {
+    #map;
+    constructor() {
+        let mapCenter = [41.3851, 2.1734]; // Coordinates for Barcelona, Spain  
+        let zoomLevel = 13; 
+        let map = L.map('map').setView(mapCenter, zoomLevel);
+        const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; OpenStreetMap contributors' }); tileLayer.addTo(map);
+    }
+    mostrarPuntInicial() {
 
     }
-    mostrarPuntInicial(){
+    mostrarPunt() {
 
     }
-    mostrarPunt(){
-        
+    borrarPunt() {
+
     }
-    borrarPunt(){
-        
-    }
-    getPosicioActual(){
-        
+    getPosicioActual() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                let lat = position.coords.latitude;
+                let lng = position.coords.longitude;
+                // Coloca un marcador en la ubicación actual del usuario
+                L.marker([lat, lng]).addTo(map)
+                    .bindPopup("Estás aquí").openPopup();
+                // Centra el mapa en la ubicación actual
+                map.setView([lat, lng], 13);
+            }, function (error) {
+                console.error("Error en la geolocalización:", error);
+            });
+        } else {
+            console.error("La geolocalización no está disponible en este navegador.");
+        }
     }
 }
+
+//Applicació
+const mapa = new Mapa()
+
+
+//Fitxer CSV
+const dragarea = document.getElementById("draganddrop")
+dragarea.addEventListener('drop', (e) => {
+    const files = e.dataTransfer.files;
+    if (files.length > 0) {
+        const file = files[0];
+        if (file.type === 'text/csv') {
+            alert("CSV carregat correctament")
+        } else {
+            alert("NO es un fitxer CSV")
+        }
+    }
+});
+
+
+//Bandera del pais
+fetch("https://restcountries.com/v3.1/alpha/ES")
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (dada) {
+        console.log(dada)
+        document.getElementById("pais").src = dada[0].flags.png
+        latitud = dada[0].latlng[0]
+        longitud = dada[0].latlng[1]
+    })
